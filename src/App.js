cedd768d93b6fec65ba06bbeb6ccdd7aba845ad2
Parth Choudhary <your-email@example.com>
2024-07-10 17:56:57 +0530

    updated commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-export default function App() {
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./Navbar";
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <Router>
+     <Routes>
+        <Route exact path="/" element={<Navbar/>} />
+        </Routes>
+    </Router>
+  );
+};
+
+export default App;
