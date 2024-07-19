106be3c75fe15fe2b9af30f5f7466c7d0c4fa898
Parth Choudhary <parthchoudhary4372@gmail.com.noreply.github.com>
2024-07-19 21:41:05 +0530

    update grid

--- src/Grid.js
+++ src/Grid.js
@@ -8,13 +8,13 @@ function Grid(){
 <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
     <div className="grid gap-4">
         <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt=""/>
+            <img className="h-full max-w-full rounded-lg" src="https://i.pinimg.com/236x/4f/2b/3c/4f2b3ce181439f7ca20d47310fcc40a9.jpg" alt=""/>
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg" alt=""/>
+            <img className="h-2/4 rounded-lg" src="https://i.pinimg.com/236x/5a/b9/68/5ab968f013d3a90d71d9db84d948b9db.jpg" alt=""/>
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt=""/>
+            <img className="h-full max-w-full rounded-lg" src="https://i.pinimg.com/236x/03/90/4c/03904cccfc6f2335a8c18dc3233cbb1e.jpg" alt=""/>
         </div>
     </div>
     <div className="grid gap-4">
